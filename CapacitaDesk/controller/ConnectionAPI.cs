8900136886a8e529f58fc70d74a9d6aa5caae616
Josemilson Júnior <junior.tn1@gmail.com>
2021-06-06 09:12:55 -0300

    Inclusao de metodo PUT em ConnectionAPI

--- CapacitaDesk/controller/ConnectionAPI.cs
+++ CapacitaDesk/controller/ConnectionAPI.cs
@@ -182,6 +182,68 @@ namespace CapacitaDesk.controller
                 return objResponse;
             }
         }
+
+        public static Object put(String rota, String json, String Token)
+        {
+            Object objResponse = null;
+
+            try
+            {
+                // Classe WebRequest: Faz uma solicitaçao para um URL
+                var requisicaoWeb = WebRequest.CreateHttp(rota.ToLower());
+
+                requisicaoWeb.Method = "PUT";
+                requisicaoWeb.Headers.Add("Authorization", "Bearer " + Token);
+
+                // Método ContentType: Define o tipo do conteudo dos dados que serao enviados
+                requisicaoWeb.ContentType = "application/json; charset=utf-8";
+
+                // Converter caracteres em um array de bytes
+                var byteArray = Encoding.UTF8.GetBytes(json);
+
+                // Atributo ContentLength: Define o tamanho do conteúdo dos dados que serão enviados
+                requisicaoWeb.ContentLength = byteArray.Length;
+
+                // Metodo GetRequestStream: retorna um objeto da classe Stream (fluxo de dados)
+                // para gravar dados no recurso da internet
+                Stream stream = requisicaoWeb.GetRequestStream();
+
+                /* Método Write: Grava um sequencia de bytes no stream atual e
+                 * avança a posição atual dentro do stream até o número de bytes gravados
+                 * Parâmetros:
+                 * 1º) Array de byte que se deseja gravar
+                 * 2º) Deslocamento de bytes no qual será iniciado a cópia de bytes
+                 * 3º) Número de bytes que serão gravados
+                 */
+                stream.Write(byteArray, 0, byteArray.Length);
+                stream.Close();
+
+
+                //GetResponse: obtem um objeto com a resposta do servidor
+                var resposta = requisicaoWeb.GetResponse();
+
+                /* Para que a resposta esteja compativel para ser deserializada devemos:
+                 * 1. Obter um stream (fluxo de bytes) as partir dos dados de resposta enviados
+                 * 2. Armazenar esta stream como um simples objeto: Classe StreamReade
+                 * 3. Ler a informacao deste objeto com metodo ReadToEnd().
+                 */
+
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                objResponse = reader.ReadToEnd();
+
+                resposta.Close();
+                reader.Close();
+
+                return objResponse;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erro ao exportar lista: " + err.Message);
+                return objResponse;
+            }
+        }
+
         public static Object remove(String rota, String json, String Token)
         {
             Object objResponse = null;
