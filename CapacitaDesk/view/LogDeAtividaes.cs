84f8cae8d491abd2472fca4eea009bdf0b6c0218
Waschislin <wlalucena@gmail.com>
2021-06-09 20:41:03 -0300

    Finalizando tela financeiro e logAtividade

--- CapacitaDesk/view/LogDeAtividaes.cs
+++ CapacitaDesk/view/LogDeAtividaes.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapacitaDesk.controller;
 using CapacitaDesk.model;
+using Newtonsoft.Json;
 
 namespace CapacitaDesk {
     public partial class LogDeAtividaes : Form {
@@ -16,6 +18,69 @@ namespace CapacitaDesk {
         public LogDeAtividaes(AdminLogado admin) {
             InitializeComponent();
             this.administrador = admin;
+            encherListView();
+        }
+
+        private void BtnBuscarAtividade_Click(object sender, EventArgs e)
+        {
+            String[] formatDate = new string[2];
+            Administrador admin = new Administrador();
+            admin.idAdministrador = TxtBoxID.Text;
+            if(!(admin.idAdministrador == null || admin.idAdministrador.Trim().Equals(""))) { 
+            List<LogAdministrador> logAdmin;
+            String rota = "http://localhost:3000/log/" + admin.idAdministrador;
+            String json = JsonConvert.SerializeObject(admin);
+            Object objResponse = ConnectionAPI.getOne(rota, json, administrador.Token);
+            logAdmin = JsonConvert.DeserializeObject<List<LogAdministrador>>(objResponse.ToString());
+            ListViewAtividade.Items.Clear();
+            foreach (LogAdministrador log in logAdmin)
+
+            {
+                ListViewItem item = ListViewAtividade.Items.Add(log.administrador.idAdministrador);
+                formatDate = log.dataHora.Split("T");
+                item.SubItems.Add(formatDate[0]);
+                item.SubItems.Add(log.administrador.nome);
+                item.SubItems.Add(log.logAtividade);
+            }
+            }
+            else
+            {
+                MessageBox.Show("Digite um ID para pesquisar!!");
+                encherListView();
+            }
+
+
+        }
+        public void encherListView()
+        {
+            String[] formatDate = new string[2];
+            List<LogAdministrador> logAdm;
+            String rota = "http://localhost:3000/log";
+            Object objectResponse = ConnectionAPI.getLista(rota, this.administrador.Token);
+            logAdm = JsonConvert.DeserializeObject<List<LogAdministrador>>(objectResponse.ToString());
+            ListViewAtividade.Items.Clear();
+            foreach(LogAdministrador log in logAdm)
+            
+            {
+                ListViewItem item = ListViewAtividade.Items.Add(log.administrador.idAdministrador);
+                formatDate = log.dataHora.Split("T");
+                item.SubItems.Add(formatDate[0]);
+                item.SubItems.Add(log.administrador.nome);
+                item.SubItems.Add(log.logAtividade);
+            }
+        }
+
+        private void TxtBoxID_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TxtBoxID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
--- CapacitaDesk/view/RegistroFinanceiro.cs
+++ CapacitaDesk/view/RegistroFinanceiro.cs
@@ -43,5 +43,87 @@ namespace CapacitaDesk {
         {
             
         }
+
+        private void BtnBuscarParceiro_Click(object sender, EventArgs e)
+        {
+            Financeiro financas = new Financeiro();
+            Instituicao inst = new Instituicao();
+            Curso curso = new Curso();
+            inst.nome = TxtBoxNomedaInstitucao.Text;
+            curso.nome_curso = textBoxNomeCurso.Text;
+            ListViewFinanceiro.Items.Clear();
+            if (!(inst.nome == null || inst.nome.Trim().Equals("")))
+            {
+               if(curso.nome_curso == null || curso.nome_curso.Trim().Equals(""))
+                {
+                    String rota = "http://localhost:3000/financeiro/instituicao";
+                    String json = JsonConvert.SerializeObject(inst);
+                    Object objResponse = ConnectionAPI.post(rota, json, administrador.Token);
+                    List<Financeiro> financeiro;
+                    financeiro = JsonConvert.DeserializeObject<List<Financeiro>>(objResponse.ToString());
+                    String[] formatDate = new string[2];
+                    double total = 0;
+                    ListViewFinanceiro.Items.Clear();
+                    foreach (Financeiro financ in financeiro)
+                    {
+                        ListViewItem item = ListViewFinanceiro.Items.Add(financ.curso.nome_curso);
+                        item.SubItems.Add(financ.usuarioPCD.nome);
+                        item.SubItems.Add(financ.instituicao.nome);
+                        formatDate = financ.dataHora.Split("T");
+                        item.SubItems.Add(formatDate[0]);
+                        total = total + Convert.ToInt32(financ.valor) * 0.5;
+                        item.SubItems.Add(financ.valor);
+                    }
+                    textBoxSaldoTotal.Text = Convert.ToString(total);
+                }
+                else
+                {
+                    financas.curso = curso;
+                    financas.instituicao = inst;
+                    String rota = "http://localhost:3000/financeiro/instituicao/cursos";
+                    String json = JsonConvert.SerializeObject(financas);
+                    Object objResponse = ConnectionAPI.post(rota, json, administrador.Token);
+                    List<Financeiro> financeiro;
+                    financeiro = JsonConvert.DeserializeObject<List<Financeiro>>(objResponse.ToString());
+                    String[] formatDate = new string[2];
+                    double total = 0;
+                    ListViewFinanceiro.Items.Clear();
+                    foreach (Financeiro financa in financeiro)
+                    {
+                        ListViewItem item = ListViewFinanceiro.Items.Add(financa.curso.nome_curso);
+                        item.SubItems.Add(financa.usuarioPCD.nome);
+                        item.SubItems.Add(financa.instituicao.nome);
+                        formatDate = financa.dataHora.Split("T");
+                        item.SubItems.Add(formatDate[0]);
+                        total = total + Convert.ToInt32(financa.valor) * 0.5;
+                        item.SubItems.Add(financa.valor);
+                    }
+                    textBoxSaldoTotal.Text = Convert.ToString(total);
+                }
+            }
+            else
+            {
+                String rota = "http://localhost:3000/financeiro/cursos";
+                String json = JsonConvert.SerializeObject(curso);
+                Object objResponse = ConnectionAPI.post(rota, json, administrador.Token);
+                List<Financeiro> financeiro;
+                financeiro = JsonConvert.DeserializeObject<List<Financeiro>>(objResponse.ToString());
+                String[] formatDate = new string[2];
+                double total = 0;
+                ListViewFinanceiro.Items.Clear();
+                foreach (Financeiro financ in financeiro)
+                {
+                    ListViewItem item = ListViewFinanceiro.Items.Add(financ.curso.nome_curso);
+                    item.SubItems.Add(financ.usuarioPCD.nome);
+                    item.SubItems.Add(financ.instituicao.nome);
+                    formatDate = financ.dataHora.Split("T");
+                    item.SubItems.Add(formatDate[0]);
+                    total = total + Convert.ToInt32(financ.valor) * 0.5;
+                    item.SubItems.Add(financ.valor);
+                }
+                textBoxSaldoTotal.Text = Convert.ToString(total);
+            }
+              
+        }
     }
 }
